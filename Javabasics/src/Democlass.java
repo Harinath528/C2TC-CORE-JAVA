decdd6b46800ef31884c09a644b573a2e51477e8
91767 <91767@DESKTOP-N47LL3B>
2022-05-26 23:57:59 +0530

    first commit

---
+++ Javabasics/src/Democlass.java
@@ -0,0 +1,18 @@
+
+public class Democlass {
+	int x;
+
+	public static void main(String[] args) {
+		Democlass myObj1 = new Democlass();
+		Democlass myObj2 = new Democlass();
+		myObj1.x = 24;
+		myObj2.x = 55;
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
+	}
+}
+		
+
+	
+
+
