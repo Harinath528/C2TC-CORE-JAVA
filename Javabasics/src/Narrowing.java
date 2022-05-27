b09c4c77412c91cd19f4dc56a42ff27ceff01dc5
91767 <91767@DESKTOP-N47LL3B>
2022-05-28 01:12:48 +0530

    first commit

---
+++ Javabasics/src/Narrowing.java
@@ -0,0 +1,11 @@
+
+public class Narrowing {
+
+	public static void main(String[] args) {
+		double mydouble=9.89;
+		int myint=(int)mydouble;
+		System.out.println(mydouble);
+		System.out.println(myint);
+	}
+
+}
