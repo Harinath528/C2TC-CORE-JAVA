316a9b69273d899873bce099528eb1c1aa8ef6cf
91767 <91767@DESKTOP-N47LL3B>
2022-05-27 23:47:33 +0530

    first commit

---
+++ Javabasics/src/Widening.java
@@ -0,0 +1,14 @@
+
+public class Widening {
+	
+
+	public static void main(String[] args) {
+		int myint=9;
+		double mydouble=myint;
+		System.out.println(mydouble);
+		System.out.println(myint);
+		
+
+	}
+
+}
