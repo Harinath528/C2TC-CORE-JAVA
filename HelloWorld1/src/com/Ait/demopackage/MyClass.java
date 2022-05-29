c7b37d73ce09933037c69439137471fe6c5377ca
91767 <91767@DESKTOP-N47LL3B>
2022-05-30 01:15:55 +0530

    first commit

---
+++ HelloWorld1/src/com/Ait/demopackage/MyClass.java
@@ -0,0 +1,10 @@
+package com.Ait.demopackage;
+
+public class MyClass {
+
+	public  void getname(String name) {
+	
+System.out.println(name);
+	}
+
+}
---
+++ HelloWorld1/src/com/Ait/demopackage2/MyPackage.java
@@ -0,0 +1,16 @@
+
+package com.Ait.demopackage2;
+
+import com.Ait.demopackage.MyClass;
+public class MyPackage {
+
+	public static void main(String[] args) {
+	String name="wellcome to my world";
+	MyClass obj=new MyClass();
+	obj.getname(name);
+	System.out.println(obj);
+	
+
+	}
+
+}
