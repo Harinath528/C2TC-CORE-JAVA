5c219ab7081ae95ac9101da4ec6ba770c5f17ec1
91767 <91767@DESKTOP-N47LL3B>
2022-05-31 22:52:37 +0530

    first commit

---
+++ string/src/string/Test2.java
@@ -0,0 +1,15 @@
+package string;
+
+public class Test2 {
+	public static void main(String[] args) {
+	String s1="harinath";
+	String s2="one piece";
+	String replaceString1=s2.replace("one","last");
+	String replaceString=s1.replace('a','@');
+	System.out.println(replaceString);
+	System.out.println(replaceString1);
+
+	
+	}
+
+}
