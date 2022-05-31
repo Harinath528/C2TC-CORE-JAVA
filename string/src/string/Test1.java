1e853097f2e2f56e4229beb4f18dc33bb84682bf
91767 <91767@DESKTOP-N47LL3B>
2022-05-31 21:51:52 +0530

    first commit

---
+++ string/src/string/Test1.java
@@ -0,0 +1,12 @@
+package string;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	String str="mugiwara no ";
+	String s=new String("luffy");
+	System.out.print(str);
+	System.out.print(s);
+	}
+
+}
